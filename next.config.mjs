4a764de5576b9315e5a7509b62c7166f9df8dcba
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2024-12-25 23:40:12 +0100

    dockerized, finaly!

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    // ignoreBuildErrors: true,
+  },
   // Your Next.js config here
 }
 
